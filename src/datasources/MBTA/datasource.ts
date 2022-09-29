547f06329e609599814e9d5ff5a96456ea395528
Mikhail <mikhail.dorokhov@outlook.com>
2022-09-29 18:47:29 -0400

    Updated datasource and q1 to fetch less info

--- src/datasources/MBTA/datasource.ts
+++ src/datasources/MBTA/datasource.ts
@@ -5,15 +5,23 @@ import { RouteData, RouteType } from "./models";
 export class MbtaApiIntegration {
     private baseUrl = "https://api-v3.mbta.com";
 
-    async getRoutesByType(routeTypes: RouteType[]) : Promise<string[]>  {
+    // Returns the Routes of the given types.
+    async getRoutesByType(routeTypes: RouteType[])  {
         const routes = await axios({
             method: "get",
-            url: `${this.baseUrl}/routes?filter[type]=${routeTypes.join(',')}`
+            url: `${this.baseUrl}/routes?fields[]=long_name,id&filter[type]=${routeTypes.join(',')}`
         });
-        
-        return routes.data.data.map((route: RouteData) => 
-        {
-            return route.attributes?.long_name;  
-        })
+        return routes.data.data;
+    }
+
+
+    // Gets the stops for each given Route
+    async getStopsByRouteIds(id: string) {
+        const line = await axios({
+            method: "get",
+            baseURL: this.baseUrl,
+            url: `/stops?route=${id}` 
+        });
+        return line;
     }
 }
--- src/question1.ts
+++ src/question1.ts
@@ -1,10 +1,15 @@
 import { MbtaApiIntegration } from "./datasources/MBTA/datasource";
-import { RouteType } from "./datasources/MBTA/models";
+import { RouteData, RouteType } from "./datasources/MBTA/models";
 
 async function main() {
     const api = new MbtaApiIntegration();
     const response = await api.getRoutesByType([RouteType.LIGHT_RAIL, RouteType.HEAVY_RAIL]);
-    console.log(response);
+
+    const longNames = response.map((route: RouteData) => 
+    {
+        return route.attributes?.long_name 
+    })
+    console.log(longNames);
 }
 
 main()
