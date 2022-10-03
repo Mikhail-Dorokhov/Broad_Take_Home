dd70a47588b1beccb76b4afe89ff5e6ae47d9b09
Mikhail <mikhail.dorokhov@outlook.com>
2022-10-03 08:57:30 -0400

    Added functionality to find the line a stop belongs to

--- src/question3.ts
+++ src/question3.ts
@@ -2,32 +2,53 @@ import { MbtaApiIntegration } from "./datasources/MBTA/datasource";
 import { RouteData, RouteType } from "./datasources/MBTA/models";
 
 async function main() {
-    const api = new MbtaApiIntegration();
+  const api = new MbtaApiIntegration();
 
-    const args = process.argv.slice(2);
+  const args = process.argv.slice(2);
 
-    //Get the stops the user passed in on the command line
-    if (args.length !== 2) {
-        throw Error("You must pass in 2 stop names, each in quotes, and seperated by a space")
+  //Get the stops the user passed in on the command line
+  if (args.length !== 2) {
+    throw Error(
+      "You must pass in 2 stop names, each in quotes, and seperated by a space"
+    );
+  }
+  const [startStop, endStop] = args;
+  let startRoute = null;
+  let endRoute = null;
+  console.log(args);
+
+  const stopsMap = await api.buildStopsToRoutesMap([
+    RouteType.LIGHT_RAIL,
+    RouteType.HEAVY_RAIL,
+  ]);
+  //find out which routes the given stops are on
+
+  stopsMap.forEach((stops, route) => {
+    if (stops.includes(startStop)) {
+      startRoute = route;
     }
-    console.log(args);
+    if (stops.includes(endStop)) {
+      endRoute = route;
+    }
+  });
 
-    //find out which routes the given stops are on
+  console.log(startRoute, endRoute);
 
-    // How to model connections between routes?
-    //      List of routes to connecting stop?
-    //      Ex: [Red, Orange]: Downtown Crossing   
+  // How to model connections between routes?
+  //      List of routes to connecting stop?
+  //      Ex: [Red, Orange]: Downtown Crossing
 
-    // What happens if lines are not directly connected?
-    // Ex: Blue to Red
-    //  Find lines that they are connected to and check for conenctions between those lines
-    // Future enhancements: Find the shortest connection between the 2 lines
+  //What happens if either the start or dest stop are on multiple lines?
+  // Ex: Park Street
+  // What happens if lines are not directly connected?
+  // Ex: Blue to Red
+  //  Find lines that they are connected to and check for connenctions between those lines
+  // Future enhancements: Find the shortest connection between the 2 lines
 
-    // What happens if lines have sections that overlap?
-    // Ex: all of the green lines
+  // What happens if lines have sections that overlap?
+  // Ex: all of the green lines
 
-    // Return a list of stops that will get you from the source to the dest
-
+  // Return a list of stops that will get you from the source to the dest
 }
 
-main()
+main();
