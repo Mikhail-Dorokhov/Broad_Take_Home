08155c3087d308f8c2936f16de3db5cee494469a
Mikhail <mikhail.dorokhov@outlook.com>
2022-10-03 08:39:05 -0400

    Reduce, Reuse, Recyclegit add src/datasources/ Oh and documentation. Documentation is important

--- src/datasources/MBTA/datasource.ts
+++ src/datasources/MBTA/datasource.ts
@@ -1,27 +1,47 @@
 import axios from "axios";
 import { RouteData, RouteType } from "./models";
 
-
 export class MbtaApiIntegration {
-    private baseUrl = "https://api-v3.mbta.com";
+  private baseUrl = "https://api-v3.mbta.com";
 
-    // Returns the Routes of the given types.
-    async getRoutesByType(routeTypes: RouteType[])  {
-        const routes = await axios({
-            method: "get",
-            url: `${this.baseUrl}/routes?fields[]=long_name,id&filter[type]=${routeTypes.join(',')}`
-        });
-        return routes.data.data;
+  // Returns the Routes of the given types.
+  async getRoutesByType(routeTypes: RouteType[]) {
+    const routes = await axios({
+      method: "get",
+      url: `${
+        this.baseUrl
+      }/routes?fields[]=long_name,id&filter[type]=${routeTypes.join(",")}`,
+    });
+    return routes.data.data;
+  }
+
+  // Gets the stops for each given Route
+  async getStopsByRouteIds(id: string) {
+    const line = await axios({
+      method: "get",
+      baseURL: this.baseUrl,
+      url: `/stops?route=${id}`,
+    });
+    return line.data.data;
+  }
+
+  // This function creates a map of Route Names to the stops along that route
+  // for the given RouteTypes
+  async buildStopsToRoutesMap(routeTypes: RouteType[]) {
+    const response = await this.getRoutesByType(routeTypes);
+
+    const routeIds: string[] = response.map((route: RouteData) => route.id);
+
+    const routeToStopNamesMap = new Map<string, string[]>();
+
+    for (const routeId of routeIds) {
+      const stops = await this.getStopsByRouteIds(routeId);
+      routeToStopNamesMap.set(
+        routeId,
+        stops.map((stop: { attributes: { name: any } }) => stop.attributes.name)
+      );
     }
 
-
-    // Gets the stops for each given Route
-    async getStopsByRouteIds(id: string) {
-        const line = await axios({
-            method: "get",
-            baseURL: this.baseUrl,
-            url: `/stops?route=${id}` 
-        });
-        return line.data.data;
-    }
+    return routeToStopNamesMap;
+  }
 }
--- src/datasources/MBTA/models.ts
+++ src/datasources/MBTA/models.ts
@@ -1,3 +1,12 @@
+/**
+ * This file contains type definitions for data that the MBTA api returns.
+ * The primary goal of these definitions is to improve developer experience 
+ * by giving TS enough context to be able to provide autocomplete suggestions.
+ * 
+ * It is important to note that since TS enforces type compatability 
+ * and not type equality these types are NOT complete. 
+ * Please consult the MBTA api for the entire sturcture of these objects 
+ */
 export enum RouteType {
     LIGHT_RAIL = 0,
     HEAVY_RAIL = 1
